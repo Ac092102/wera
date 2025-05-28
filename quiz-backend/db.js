455baa799b54139dcfc42266fdf5df4ff50e69fa
Ac092102 <ajcangmaong@paterostechnologicalcollege.edu.ph>
2025-05-29 06:06:34 +0800

    Update db.js

--- quiz-backend/db.js
+++ quiz-backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '', // Use your MySQL password if any
+  password: '',
   database: 'quiz_app'
 });
 
